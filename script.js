d122677dcacf9b6a91025106966ea9781a0526ea
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-30 22:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function stringChop(str, size) {
+	
+	let str2=""
+	let arr=[]
+	for(let i=0;i<str.length;i++)
+		{
+			if(str.length<size)
+			{
+				return str
+			}
+			else if(str2.length==size)
+			{
+				arr.push(str2)
+				str2=str[i]
+			}
+			else
+			{
+				str2+=str[i];
+			}
+		}
+	if(str.length>0)
+	{
+		arr.push(str2)
+	}
+	return arr
   // your code here
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
